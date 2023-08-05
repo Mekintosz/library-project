a71d5b1179bcb1a5e455392e4e926baa467beb46
Mekintosz <michal.lazicki@gmail.com>
2023-08-05 23:08:21 +0200

    Set the files, add prototype for book, push to library

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToLibrary() {
+    myLibrary.push()
+  const book1 = new Book("Beksinski Fotografia", "Wieslaw Banach", 64)
+}
